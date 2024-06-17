0d3ef0c9f5fc1b4d029907d7c0d65f8e20101493
Tanko <john.tanko@falcontechnologies.com.gh>
2024-06-17 20:07:33 +0000

    fix

--- src/app/pages/explore.tsx
+++ src/app/pages/explore.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import HorizontalNav from '../components/HorizontalNav'
 
-const ExplorePage = () => {
+const Explore = () => {
   return (
     <div>
       <HorizontalNav />
@@ -9,4 +9,4 @@ const ExplorePage = () => {
   )
 }
 
-export default ExplorePage
+export default Explore
